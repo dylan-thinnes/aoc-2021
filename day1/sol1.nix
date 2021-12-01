0106b22cf1e4d89278dfb758ac919fd7e3667101
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-01 14:11:50 +0000

    Solve day 1 in Nix

---
+++ day1/sol1.nix
@@ -0,0 +1,10 @@
+with builtins;
+with (import <nixpkgs> {}).lib;
+
+rec {
+    input = readFile ./input;
+    words = filter (x: isString x && stringLength x > 0) (split "\n" input);
+    numbers = map fromJSON words;
+    gt-last = zipListsWith (x: y: x < y) numbers (tail numbers);
+    gt-last-count = length (filter id gt-last);
+}
---
+++ day1/sol2.nix
@@ -0,0 +1,11 @@
+with builtins;
+with (import <nixpkgs> {}).lib;
+
+rec {
+    input = readFile ./input;
+    words = filter (x: isString x && stringLength x > 0) (split "\n" input);
+    numbers = map fromJSON words;
+    sum3 = zipListsWith add (zipListsWith add numbers (tail numbers)) (tail (tail numbers));
+    gt-last = zipListsWith lessThan sum3 (tail sum3);
+    gt-last-count = length (filter id gt-last);
+}
