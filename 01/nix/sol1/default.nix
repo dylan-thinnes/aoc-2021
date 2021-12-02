f7057b55a94bbc98ab877ab819a41e12dd67b628
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-02 23:52:00 +0000

    Summarize prints if sol1/sol2 have been done independently

--- 01/nix/sol1/default.nix
+++ 01/nix/sol1/default.nix
@@ -2,7 +2,7 @@ with builtins;
 with (import <nixpkgs> {}).lib;
 
 rec {
-    input = readFile ../input;
+    input = readFile ../../input;
     words = filter (x: isString x && stringLength x > 0) (split "\n" input);
     numbers = map fromJSON words;
     gt-last = zipListsWith (x: y: x < y) numbers (tail numbers);
--- 01/nix/sol2/default.nix
+++ 01/nix/sol2/default.nix
@@ -2,7 +2,7 @@ with builtins;
 with (import <nixpkgs> {}).lib;
 
 rec {
-    input = readFile ../input;
+    input = readFile ../../input;
     words = filter (x: isString x && stringLength x > 0) (split "\n" input);
     numbers = map fromJSON words;
     sum3 = zipListsWith add (zipListsWith add numbers (tail numbers)) (tail (tail numbers));
