42cceee2e989bef9fbf3b6cbe9be92b6a2c5b9b8
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-06 12:11:05 +0000

    Day 6, ruby sol

---
+++ 06/ruby/sol2.rb
@@ -0,0 +1,19 @@
+inp = $stdin.read.split(",").map { |x| x.to_i }
+
+pop = Hash.new 0
+inp.each { |x| pop[x] += 1 }
+
+(1..256).each do |idx|
+  new_pop = Hash.new 0
+  pop.each do |key, value|
+    if key == 0
+      new_pop[8] += value
+      new_pop[6] += value
+    else
+      new_pop[key - 1] += value
+    end
+  end
+  pop = new_pop
+end
+
+puts pop.values.sum
