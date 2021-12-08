5584ec1c69eda800a8868cf2ced3f3c1ee515492
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-08 18:39:59 +0000

    Vectorized/matrixed day 7 solution

--- 07/julia/sol1.jl
+++ 07/julia/sol1.jl
@@ -4,19 +4,14 @@ inp = readdlm("../input", ',', Int)
 
 # Tally crab locations
 m = maximum(inp)+1
-locations = zeros(Int, m)
+crabs = zeros(Int, m)
 for i in inp
-  locations[i+1] += 1
+  crabs[i+1] += 1
 end
 
-# Distance travelled
-best = Inf
-best_idx = 0
-for i in 1:m
-  distances = abs.((1:m) .- i)
-  travelled = sum(distances .* locations)
-  if travelled < best
-    global best = travelled
-    global best_idx = i
-  end
-end
+# Calculate distance travelled for each candidate location
+distances = abs.(repeat(1:m, inner=(1,m))' .- (1:m))
+travelled = distances * crabs
+
+# Print the best
+println(minimum(travelled))
