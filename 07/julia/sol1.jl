2f0222583eeb1ab72a4226266db6ab5ca55cfd35
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-08 19:02:42 +0000

    Improve julia solution to be even cooler

--- 07/julia/sol1.jl
+++ 07/julia/sol1.jl
@@ -5,9 +5,7 @@ inp = readdlm("../input", ',', Int)
 # Tally crab locations
 m = maximum(inp)+1
 crabs = zeros(Int, m)
-for i in inp
-  crabs[i+1] += 1
-end
+view(crabs, inp .+ 1) .+= 1
 
 # Calculate distance travelled for each candidate location
 distances = abs.(repeat(1:m, inner=(1,m))' .- (1:m))
--- 07/julia/sol2.jl
+++ 07/julia/sol2.jl
@@ -5,13 +5,11 @@ inp = readdlm("../input", ',', Int)
 # Tally crab locations
 m = maximum(inp)+1
 crabs = zeros(Int, m)
-for i in inp
-  crabs[i+1] += 1
-end
+view(crabs, inp .+ 1) .+= 1
 
 # Calculate distance travelled for each candidate location
 distances = abs.(repeat(1:m, inner=(1,m))' .- (1:m))
-distances = (distances .* distances .+ distances) .÷ 2
+distances = (distances .^ 2 .+ distances) .÷ 2
 travelled = distances * crabs
 
 # Print the best
