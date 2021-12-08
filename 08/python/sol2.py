580e4061f46d31f987ca76bd94c583551571e221
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-08 20:41:34 +0000

    Clean up 08/Python

--- 08/python/sol2.py
+++ 08/python/sol2.py
@@ -3,12 +3,7 @@ total = 0
 for l in sys.stdin.readlines():
     l = l[:-1]
     pres, posts = l.split(" | ")
-    digits = []
 
-    one = None
-    four = None
-    seven = None
-    eight = None
     for pre in pres.split(" "):
         if len(pre) == 2:
             one = set(pre)
@@ -16,36 +11,35 @@ for l in sys.stdin.readlines():
             four = set(pre)
         elif len(pre) == 3:
             seven = set(pre)
-        elif len(pre) == 7:
-            eight = set(pre)
 
+    result = 0
     for post in posts.split(" "):
         if len(post) == 2:
-            digits.append(1)
+            digit = 1
         elif len(post) == 4:
-            digits.append(4)
+            digit = 4
         elif len(post) == 3:
-            digits.append(7)
+            digit = 7
         elif len(post) == 7:
-            digits.append(8)
+            digit = 8
         elif len(post) == 5:
             if seven.issubset(post):
-                digits.append(3)
+                digit = 3
             elif four.difference(one).issubset(post):
-                digits.append(5)
+                digit = 5
             else:
-                digits.append(2)
+                digit = 2
         elif len(post) == 6:
             if four.issubset(post):
-                digits.append(9)
+                digit = 9
             elif seven.issubset(post):
-                digits.append(0)
+                digit = 0
             else:
-                digits.append(6)
+                digit = 6
         else:
             raise Exception(f"Invalid input with {len(post)} digits!")
 
-    result = int("".join([str(d) for d in digits]))
-    print(result)
+        result = result * 10 + digit
+
     total += result
 print(total)
