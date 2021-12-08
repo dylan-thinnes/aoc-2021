da39b557d7dd7dadd8c9853eb9d5dee669048010
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-08 19:25:22 +0000

    Improve day 7 to increment indices at read time

--- 07/julia/sol1.jl
+++ 07/julia/sol1.jl
@@ -1,11 +1,11 @@
 # Load input
 using DelimitedFiles
-inp = readdlm("../input", ',', Int)
+inp = readdlm("../input", ',', Int) .+ 1
 
 # Tally crab locations
-m = maximum(inp)+1
+m = maximum(inp)
 crabs = zeros(Int, m)
-view(crabs, inp .+ 1) .+= 1
+view(crabs, inp) .+= 1
 
 # Calculate distance travelled for each candidate location
 distances = abs.(repeat(1:m, inner=(1,m))' .- (1:m))
