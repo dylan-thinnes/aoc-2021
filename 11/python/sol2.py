3cf01ac715ca38886b569d4f13e6142f52e7e962
Dylan Thinnes <dylan.thinnes@protonmail.com>
2021-12-11 13:45:03 +0000

    Improve Day11/Python/Sol2

--- 11/python/sol2.py
+++ 11/python/sol2.py
@@ -1,57 +1,42 @@
-fh = open("../input")
-inp = [[[int(c)] for c in list(line[:-1])] for line in fh.readlines()]
+import sys
 
-height = len(inp)
-width = len(inp[0])
+# Setup global state
+state = [[[int(c)] for c in list(line[:-1])] for line in sys.stdin.readlines()]
+height = len(state)
+width = len(state[0])
 
-def pretty():
-    print()
-    for row in inp:
-        for val in row:
-            if val[0] is None:
-                print("█", end="")
-            else:
-                print(f"{val[0]:x}", end="")
-        print()
-    print()
-
-#pretty()
-
-flashes = 0
+# Step state forward by 1
 def step():
-    global inp, flashes
+    global state
 
-    frontier = set()
-    for y, row in enumerate(inp):
+    flashes = set()
+
+    # Increment everyone, find fresh flashes
+    for y, row in enumerate(state):
         for x, val in enumerate(row):
             val[0] += 1
-            if val[0] is not None and val[0] > 9:
-                frontier.add((x, y))
+            if val[0] > 9:
+                flashes.add((x, y))
+                val[0] = None
 
-    #pretty()
-
-    while len(frontier) > 0:
-        new_frontier = set()
-        for x,y in frontier:
-            val = inp[y][x]
-            val[0] = None
-
-        for x,y in frontier:
-            val = inp[y][x]
-            flashes += 1
-            for dx in [-1,0,1]:
-                for dy in [-1,0,1]:
-                    if 0 <= (y+dy) < height and 0 <= (x+dx) < width:
-                        val = inp[y+dy][x+dx]
+    # Increment neighbours until no flashes are produced
+    while len(flashes) > 0:
+        next_flashes = set()
+        for cx, cy in flashes:
+            val = state[cy][cx]
+            for x in [cx-1, cx, cx+1]:
+                for y in [cy-1, cy, cy+1]:
+                    if 0 <= y < height and 0 <= x < width:
+                        val = state[y][x]
                         if val[0] is not None:
                             val[0] += 1
                             if val[0] > 9:
-                                new_frontier.add((x+dx, y+dy))
-        frontier = new_frontier
+                                next_flashes.add((x, y))
+                                val[0] = None
+        flashes = next_flashes
 
-    #pretty()
-
-    for row in inp:
+    # Turn all flashes into zeros
+    for row in state:
         for val in row:
             if val[0] is None:
                 val[0] = 0
@@ -59,10 +44,9 @@ def step():
 i = 1
 while True:
     print(f"Step {i}")
-    i += 1
     step()
-    simul = all([val[0] == 0 for row in inp for val in row])
-    if simul:
+    if all([val[0] == 0 for row in state for val in row]):
         break
+    i += 1
 
-print(flashes)
+print(f"Final step: {i}")
